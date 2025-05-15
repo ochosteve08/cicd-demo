2d2b6f38a2c044b97ac25c66cf01bef940b7415f
Ujah Stephen Ocheola <stephenujah@yahoo.com>
2025-05-15 16:54:44 +0100

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to GitHub Action');
+});
+
+
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
